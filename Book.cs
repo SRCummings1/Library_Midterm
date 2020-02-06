14f0d01743b3f0a8c07c7f3891032865ffaeba46
Sean <srcummings85@gmail.com>
2020-02-06 15:48:40 -0500

    first commit of skeleton for Midterm

---
+++ Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library_Terminal_Midterm
+{
+    class Book
+    {
+
+        public static List<Book> Books = new List<Book>()
+        {
+            new Book("The Hobbit", "J.R.R. Tolkien", "Fantasy"),
+            new Book("Fellowship of the Ring","J.R.R. Tolkien", "Fantasy"),
+            new Book("The Two Towers","J.R.R. Tolkien", "Fantasy" )
+        };     
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public bool Status  { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public Book()
+        {
+            Status = true;
+        }
+        public Book(string title, string author, string genre, bool status = true)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Status = status;
+        }
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Library_Terminal_Midterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Class Program -Logic- (Promps, Inputs, Outputs)(If statements, Switch statements)
+
+            Console.WriteLine("Hello, welcome to the Library.");
+            DisplayMenu();//Calling the Method
+            GetUserInput("Please Make A Selection From Above: ");
+
+
+        }
+        public static void DisplayMenu()
+        {
+            Console.WriteLine(" [1]: Display All Books \n [2]: Display Books by Author \n [3]: Display Books by Title \n [4]: Check out Book \n [5] Leave Library ");
+
+        }
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+
+
+        }
+    }
+}
